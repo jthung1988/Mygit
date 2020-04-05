3b9ec1fbb8b370bbd052d59c9d2b61390a6bd6ec
Jimmy <next.on7739@mas.hinet.net>
2020-04-05 23:20:22 +0800

    Sorry, I sort directory and new some description file Detail: 1.SQL:Table:Profile & ProfileDetail 2.JAVA:a.JNDI,Hibernate configure 2.JAVA:b.Profile,ProfileDetail (Bean,Dao,Service) 2.JAVA:c.Test jsp,controller

---
+++ Readme.txt
@@ -0,0 +1,33 @@
+JAVA檔案:ThirdTeamProject
+
+一、
+開啟Eclipse，Workspace直接選擇ThirdTeamProject
+
+
+或是
+二、
+直接	import>>General>>Projects from Folder or Archive>>next
+	右上Directory選擇 ThirdTeamProject資料夾
+	選擇 Servers(註1)和Valckth
+	finish
+
+註1:這是Tomcat的資料夾，不確定行不行，不行的話先自己建立Tomcat，
+    並修改context.xml如下:
+	<Context>
+	<Resource
+	  url="jdbc:sqlserver://localhost:1433;databaseName=Valckth"
+	  driverClassName="com.microsoft.sqlserver.jdbc.SQLServerDriver"
+	  username="sa"
+*要改>>	  password="1qaz2wsx"
+	  auth="Container"
+	  type="javax.sql.DataSource"
+	  name="connectSqlServerJdbc/Valckth"/>
+
+
+
+	........</Context>
+==============================================================
+網頁如用其他IDE設計可放在根目錄下，搭配TempCSS和TempJS，設計好之後再丟進WebContainer
+
+==========
+valckth是我挑七個人的名字亂取的(七大醉?)，以後配合好聽的專案名字做修改
---
+++ ThirdTeamProject/Valckth/src/tw/valckth/user/model/P_ProfileService.java
@@ -0,0 +1,35 @@
+package tw.valckth.user.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class P_ProfileService {
+	
+	private P_ProfileDao profileDao;
+
+	public P_ProfileService(){
+	}
+	
+	@Autowired
+	public P_ProfileService(P_ProfileDao profileDao){
+		this.profileDao = profileDao;
+	}
+	
+	public P_Profile createProfile(P_Profile profile, PD_ProfileDetail profileDetail) {
+		return profileDao.createProfile(profile, profileDetail);
+	}
+	
+	public P_Profile queryProfile(String userId) {
+		return profileDao.queryProfile(userId);
+	}
+	
+	public boolean updateProfile(P_Profile profile) {
+		return profileDao.updateProfile(profile);
+	}
+	
+	public boolean updateProfile(P_Profile profile, PD_ProfileDetail profileDetail) {
+		return profileDao.updateProfile(profile, profileDetail);
+	}
+
+}
